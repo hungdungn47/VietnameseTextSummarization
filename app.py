fc3820994132b950c7e7af36d5d9d3668d5f9a1c
hungdungn47 <hungdungn47@gmail.com>
2024-07-31 22:13:05 +0700

    capitalize tieng Viet

--- app.py
+++ app.py
@@ -2,10 +2,7 @@ import streamlit as st
 from io import StringIO
 from chdg_inference import infer
 
-st.title("Tóm tắt văn bản tiếng việt")
-# text_input = st.text_area(label="Nhập văn bản cần tóm tắt", height=200)
-# st.write("Hoặc tải file văn bản lên: ")
-st.subheader("Upload file văn bản cần tóm tắt:")
+st.title("Tóm tắt văn bản tiếng Việt")
 uploaded_file = st.file_uploader(label="Chọn file văn bản")
 
 category = st.selectbox("Chọn chủ để của văn bản: ", ['Giáo dục', 'Giải trí - Thể thao', 'Khoa học - Công nghệ', 'Kinh tế', 'Pháp luật', 'Thế giới', 'Văn hóa - Xã hội', 'Đời sống'])

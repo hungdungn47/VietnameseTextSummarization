eb6423a1c09051d0115dfff4f38a4da87368c09d
hungdungn47 <hungdungn47@gmail.com>
2024-08-06 22:57:15 +0700

    push data and display input and output in 2 columns

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import streamlit as st
 from io import StringIO
 from chdg_inference import infer
 
-st.title("Tóm tắt đa văn bản tiếng Việt")
+st.set_page_config(layout="wide")
+st.title("Tóm tắt Đa văn bản Tiếng Việt")
+
+col1, col2 = st.columns([2.5, 1])
 
 # Initialize session state
 if 'num_docs' not in st.session_state:
@@ -16,11 +19,11 @@ def add_text_area():
 
 
 # Button to add a new text area
-st.button("Thêm văn bản", on_click=add_text_area)
+col1.button("Thêm văn bản", on_click=add_text_area)
 
 # Display text areas for document input
 for i in range(st.session_state.num_docs):
-    doc = st.text_area(f"Văn bản {i+1}", key=f"doc_{i}", height=200)
+    doc = col1.text_area(f"Văn bản {i+1}", key=f"doc_{i}", height=150)
     doc.replace('\r', '\n')
     doc.replace('\"', "'")
     if len(st.session_state.docs) <= i:
@@ -28,12 +31,12 @@ for i in range(st.session_state.num_docs):
     else:
         st.session_state.docs[i] = doc
 
-category = st.selectbox("Chọn chủ để của văn bản: ", ['Giáo dục', 'Giải trí - Thể thao', 'Khoa học - Công nghệ', 'Kinh tế', 'Pháp luật', 'Thế giới', 'Văn hóa - Xã hội', 'Đời sống'])
+category = col1.selectbox("Chọn chủ để của văn bản: ", ['Giáo dục', 'Giải trí - Thể thao', 'Khoa học - Công nghệ', 'Kinh tế', 'Pháp luật', 'Thế giới', 'Văn hóa - Xã hội', 'Đời sống'])
 
 def summarize():
     summ, _ = infer(st.session_state.docs, category)
-    st.subheader("Kết quả")
-    st.write(summ)
+    col2.subheader("Kết quả")
+    col2.write(summ)
     
-if st.button("Tóm tắt"):
+if col1.button("Tóm tắt"):
     summarize()
